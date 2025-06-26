8699892089413437f3d45626172fa7daa6e255bc
Asish-2004-ai <asishdalabehera375@gmail.com>
2025-06-26 18:59:31 +0530

    Nav changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { Box } from '@mui/material';
 import AddVehiclePage from './pages/AddVehiclePage';
 import SearchPage from './pages/SearchPage';
 import MyBookingsPage from './pages/myBookingPage';
@@ -6,16 +7,16 @@ import MyBookingsPage from './pages/myBookingPage';
 function App() {
   return (
     <Router>
-      <nav>
+      <Box sx={{ display: 'flex', gap: 2, p: 2 }}>
         <Link to="/">Add Vehicle</Link>
         <Link to="/search">Search & Book</Link>
-         <Link to="/my-bookings">My Bookings</Link>
-      </nav>
+        <Link to="/my-bookings">My Bookings</Link>
+      </Box>
+
       <Routes>
         <Route path="/" element={<AddVehiclePage />} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/my-bookings" element={<MyBookingsPage />} />
-
       </Routes>
     </Router>
   );
